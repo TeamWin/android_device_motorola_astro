bcbd409c98bde25a5c0e75c16403bd661fecf4b6
Fraaxius <franthekillaz@gmail.com>
2024-08-11 23:22:39 +0000

    astro: Import modules from RPLS31.Q2-63-10-2-9
    Co-authored-by: Deivid21 <david.parra.ignacio@gmail.com>
    Signed-off-by: Fraaxius <franthekillaz@gmail.com>

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -8,10 +8,9 @@ firmware_path=/vendor/firmware
 firmware_file=
 
 # Load TouchScreen Modules
-insmod $module_path/himax_v3_mmi_hx83102d.ko
-insmod $module_path/himax_v3_mmi.ko
-insmod $module_path/ili9882_mmi.ko
-insmod $module_path/chipone_tddi_mmi.ko
+insmod $module_path/goodix_fod_mmi.ko
+insmod $module_path/himax_v2_mmi_hx83102d.ko
+insmod $module_path/himax_v2_mmi.ko
 insmod $module_path/nova_0flash_mmi.ko
 
 # Load Needed Modules
@@ -23,17 +22,6 @@ insmod $module_path/mmi_info.ko
 insmod $module_path/mmi_sys_temp.ko
 insmod $module_path/moto_f_usbnet.ko
 insmod $module_path/qpnp-power-on-mmi.ko
-insmod $module_path/qpnp_adaptive_charge.ko
+insmod $module_path/qpnp-smbcharger-mmi.ko
 
-# Fix up chipone
-if [[ -d /sys/class/touchscreen/ICNL9911S ]]; then
-        echo "chipone"
-        chipone_fw_path=/vendor/firmware/ICNL9911.bin
-        flash_path=/sys/chipone-tddi/cts_firmware
-
-        sleep 2
-
-        echo $chipone_fw_path > $flash_path/update_from_file
-fi
-
-exit 0
+return 0
